afdb484806cf840550c77145fea80d6f6fee1899
dhanugiri007 <dhanashrichaudhari200@gmail.com>
2026-05-24 09:47:52 +0530

    mistake cleared

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cookie = require('cookie-parser');
 
 const authRouter = require('./routes/auth.route');
 const patientRouter = require('./routes/patient.route');
-
+const appointmentRouter = require('./routes/appointment.route');
 const app = express();
 
 app.use(express.json());
@@ -12,5 +12,6 @@ app.use(cookie());
 
 app.use('/api/auth', authRouter);
 app.use('/api/patient', patientRouter);
+app.use('/api/appointment',appointmentRouter);
 
 module.exports = app;
